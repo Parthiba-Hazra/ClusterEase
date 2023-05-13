8512235c382502003cc4c2ce7dbda418b425f0cf
Parthiba-Hazra <parthibahazra02@gmail.com>
2023-05-13 22:52:14 +0530

    try to add kubernetes authentication
    Signed-off-by: Parthiba-Hazra <parthibahazra02@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # ClusterEase
-A command-line tool in Go that provides information about a Kubernetes cluster, allows users to scale the cluster, manage resource quotas, and perform other cluster-level operations.
+A command-line tool in Go that provides information about Kubernetes cluster, allows users to scale the cluster, manage resource quotas, and perform other cluster-level operations.
--- main.go
+++ main.go
@@ -1,11 +1,36 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "github.com/Parthiba-Hazra/ClusterEase/cmd"
+import (
+	"context"
+	"fmt"
+
+	"github.com/Parthiba-Hazra/ClusterEase/cmd"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
 
 func main() {
+	config, err := clientcmd.BuildConfigFromFlags("", "path/to/kubeconfig")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	clientSet, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	pods, err := clientSet.CoreV1().Pods("default").List(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for _, pod := range pods.Items {
+		fmt.Printf("Pod: %s\n", pod.ObjectMeta.Name)
+	}
+
 	cmd.Execute()
 }
